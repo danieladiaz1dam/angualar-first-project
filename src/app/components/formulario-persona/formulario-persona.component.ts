16dea0ede40682e8a4c459b91e883fc55fe7a87b
LaDaniWapa <urpopolakliente@gmail.com>
2025-01-16 14:32:27 +0100

    new components

---
+++ src/app/components/formulario-persona/formulario-persona.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-persona',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './formulario-persona.component.html',
+  styleUrl: './formulario-persona.component.css'
+})
+export class FormularioPersonaComponent {
+
+}
---
+++ src/app/components/tabla-persona/tabla-persona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-persona',
+  imports: [],
+  templateUrl: './tabla-persona.component.html',
+  styleUrl: './tabla-persona.component.css'
+})
+export class TablaPersonaComponent {
+
+}
